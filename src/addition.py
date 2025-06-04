36dc17169741cfb41a148ab134992d9f8c87daa1
Kotha Jagadeesh <kothajagadeesh87@gmail.com>
2025-06-04 16:43:25 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,3 +1,4 @@
+#!/bin/bash
 # app.py
 # This is a test commit
 def add(a, b, c):
@@ -5,4 +6,6 @@ def add(a, b, c):
 
 def test_add():
     assert add(1, 2, 3) == 6
-    assert add(1, -1, 2) == 0
+    assert add(1, -1, 2) == 2
+    print("all tests passed!")
+test_add() #call the test function
